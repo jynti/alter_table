ae68c9a3391afccb23706e63c0ceb757e5fae42f
jynti <33417806+jynti@users.noreply.github.com>
2018-01-08 12:33:59 +0530

    Update alter_table.sql

--- alter_table.sql
+++ alter_table.sql
@@ -1,14 +1,14 @@
-mysql> use test;
+mysql> USE test;
 Database changed
-mysql> create table testing_table
+mysql> CREATE TABLE testing_table
     -> (
-    -> name varchar(20),
-    -> contact_name varchar(20),
-    -> roll_no varchar(20)
+    -> name VARCHAR(20),
+    -> contact_name VARCHAR(20),
+    -> roll_no VARCHAR(20)
     -> );
 Query OK, 0 rows affected (0.41 sec)
 
-mysql> describe testing_table;
+mysql> DESCRIBE testing_table;
 +--------------+-------------+------+-----+---------+-------+
 | Field        | Type        | Null | Key | Default | Extra |
 +--------------+-------------+------+-----+---------+-------+
@@ -19,16 +19,17 @@ mysql> describe testing_table;
 3 rows in set (0.00 sec)
 
 
-mysql> alter table testing_table
-    -> drop name,
-    -> change contact_name username varchar(20),
-    -> add first_name varchar(20),
-    -> add last_name varchar(20),
-    -> change roll_no roll_no int;
+mysql> ALTER TABLE testing_table
+    -> DROP name,
+    -> CHANGE contact_name username VARCHAR(20),
+    -> ADD first_name VARCHAR(20),
+    -> ADD last_name VARCHAR(20),
+    -> CHANGE roll_no roll_no INT
+    -> ;
 Query OK, 0 rows affected (1.18 sec)
 Records: 0  Duplicates: 0  Warnings: 0
 
-mysql> describe testing_table;
+mysql> DESCRIBE testing_table;
 +------------+-------------+------+-----+---------+-------+
 | Field      | Type        | Null | Key | Default | Extra |
 +------------+-------------+------+-----+---------+-------+
